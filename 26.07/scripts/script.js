200196f83bfc0108a2e81963a4b2c51a2adbf981
Your Name <you@example.com>
2022-07-26 22:50:19 +0300

    This is my fifth push

---
+++ 26.07/scripts/script.js
@@ -0,0 +1,67 @@
+/*alert("Hello World");
+
+console.log("failed to open the specified link")
+*/
+//console.error("This is the error")
+
+//let c;
+//let a = 2;
+//let b = 4;
+//console.log(a);
+//a = 102;
+//console.log(a);
+//console.log(b);
+//console.log(123);
+
+//"use strict";
+
+var var1 = 123;
+console.log(var1); {
+var var1=189;
+console.log(var1);
+}
+console.log(var1);
+
+let var2 = 123;
+console.log(var2); {
+    let var2 = 189;
+    let var3 = 90;
+    let var4 = 90;
+    console.log(var2);
+}
+console.log(var2);
+// console.log(var3);
+
+let a = 5;
+let b = 8;
+let result = a + b;
+console.log(result);
+
+let name = "Liva";
+let lastName = "Stalmane";
+result = name + lastName;
+console.log(result);
+result = a + name;
+console.log(result);
+
+let element = "10";
+let element1 = "101";
+result = element + element1;
+console.log(result);
+result = element - element1;
+console.log(result);
+result = name - element;
+console.log(result);
+
+element = "10.45";
+element1 = "101,23";
+result = Number.parseInt(element) + Number.parseInt(element1);
+console.log(result);
+
+element = 123456;
+element1 = 2;
+result = element / element1;
+console.log("The result is"+result);
+
+result = element % element1;//7 % 3 = 1
+console.log("The result is" + result);
---
+++ 26.07/scripts/testscript.js
@@ -0,0 +1,21 @@
+let val1 = prompt("Your date of birth");
+let val2 = prompt("Your month of birth");
+
+result = +val1 + +val2;
+console.log("The addition result is" + " " + result);
+
+result = val1 - val2;
+console.log("The subtraction result is" + " " + result);
+
+result = val1 * val2;
+console.log("The multiplication result is" + " " + result);
+
+result = val1 / val2;
+console.log("The division result is" + " " + result);
+
+let val3 = prompt("Your name");
+
+let val4 = prompt("Your surname");
+
+
+alert("Hello" + " " + val3 +" " + val4);
