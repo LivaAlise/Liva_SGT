42273f471660a60e9b5406ad6fd7799d50601d9d
Your Name <you@example.com>
2022-07-30 11:57:31 +0300

    This is my second commit

--- 28.07/testscript.js
+++ 28.07/testscript.js
@@ -1,3 +1,7 @@
+// Task 1: Write the script which asks user to provide two values and the operation (+,-.*,/). Based on the operation value, return the result
+// Remark: If one of the values provided is not the number (string only), then display error message for the operators -,*,/ (allow concatenation only) 
+// Remark 2: Display the error message if the operation provided is not one of these (+,-.*,/)
+
 let val1 = prompt("Your date of birth");
 let val2 = prompt("Your month of birth");
 operation = prompt("What operation should I perform? (please enter + - * /)");
@@ -18,10 +22,23 @@ else if (operation == "/") {
   result = val1 / val2;
   alert("The division result is " + " " + result);
 }
+else {
+  alert("Error! Please choose one of the provided options!");
+}
 
-if (val1 || val2 == !Number) {
-  alert("ERROR!!")
-  }
-else if ( operation !== "+" || "-" || "*" || "/") {
-  alert("Error! Please enter + - * or /");
-  	}
+
+/*if (val1|val2==(!NaN){
+ alert ("Error! Please enter a numerical value!")
+}
+
+
+/*
+|  val1  |  val2  | op | result  |
+|--------|--------|----|---------|
+| number | number | +  | n1 + n2 |
+| string | string | +  | "s1s2"  |  
+*/
+
+// if (val1 || val2 == !Number) {
+//   alert("ERROR!!")
+// }
