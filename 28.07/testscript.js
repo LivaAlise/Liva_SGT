d45d728b09aa68f5d5a51b6c69e7053c043b7c66
Your Name <you@example.com>
2022-08-01 10:26:27 +0300

    This is my 2nd commit

--- 28.07/testscript.js
+++ 28.07/testscript.js
@@ -7,21 +7,48 @@ let val2 = prompt("Your month of birth");
 operation = prompt("What operation should I perform? (please enter + - * /)");
 
 if (operation == "+") {
-  result = +val1 + +val2;
-  alert("The addition result is " + " " + result);
+  if (+val1!= NaN && +val2 != NaN) {
+    result = +val1 + +val2;
+    alert("The addition result is " + " " + result);
+  }
+  else if (val1 || val2 == !number) {
+    result = val1 + val2;
+  }
 }
+
+
 else if (operation == "-") {
-  result = val1 - val2;
-  alert("The subtraction result is " + " " + result);
+  if (val1 || val2 == number) {
+    result = val1 - val2;
+    alert("The subtraction result is " + " " + result);
+  }
+  else {
+    alert("Please enter one of the operations - * / ")
+  }
 }
+
+
 else if (operation == "*") {
-  result = val1 * val2;
-  alert("The multiplication result is " + " " + result);
+  if (val1 || val2 == number) {
+    result = val1 * val2;
+    alert("The multiplication result is " + " " + result);
+  }
+  else {
+    alert("Please enter one of the operations - * / ")
+  }
 }
+
 else if (operation == "/") {
-  result = val1 / val2;
-  alert("The division result is " + " " + result);
+  if (val1 || val2 == number) {
+    result = val1 / val2;
+    alert("The division result is " + " " + result);
+  }
+  else {
+    alert("Please enter one of the operations - * / ");
+  }
 }
+
+
 else {
   alert("Error! Please choose one of the provided options!");
 }
@@ -36,7 +63,7 @@ else {
 |  val1  |  val2  | op | result  |
 |--------|--------|----|---------|
 | number | number | +  | n1 + n2 |
-| string | string | +  | "s1s2"  |  
+| string | string | +  | "s1s2"  |
 */
 
 // if (val1 || val2 == !Number) {
