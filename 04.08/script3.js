88e4f0e0d72a4b932662c24e99bdaa828833a568
Your Name <you@example.com>
2022-08-08 16:17:21 +0300

    3rd commit

--- 04.08/script3.js
+++ 04.08/script3.js
@@ -1,9 +1,9 @@
 /*3)Write a program where multiple actions with an array will be done. For element filtering please use the loop (the loop type can be chosen as You want).
 ●    Create an array with the elements from input - Y
-●    Print out elements which are negative. 
-●    Print out elements which are odd (uneven). 
-●    Compute how many elements are odd (uneven).
-●    Verify if there are multiple elements with the same value
+●    Print out elements which are negative. Y
+●    Print out elements which are odd (uneven). Y 
+●    Compute how many elements are odd (uneven). Y
+●    Verify if there are multiple elements with the same value Y
 ●    Print out each second element.
 ●    Compute average value of the array.
 ●    Establish how many elements are smaller than average value.*/
@@ -25,11 +25,43 @@ while (true) {
     else {
         alert("Please continue");
     }
-    
+
     a.push(element);
 }
 alert(a)
 
-if (a<0) {
-    alert("Error! The number entered is negative!"+ a)
+let negativesArray = [];
+for (let number of a) {
+    if (number < 0)
+        negativesArray.push(number);
 }
+alert("Negative values entered: " + negativesArray);
+
+
+let oddsArray = [];
+for (let number of a) {
+    if (number % 2 != 0)
+        oddsArray.push(number);
+}
+alert("Odd values entered: " + oddsArray);
+
+{
+    let count = 0;
+    for (let number of a) {
+        if (number % 2 != 0)
+            count++;
+    }
+    alert("You have entered " + count + " odd elements");
+}
+
+let multiplesArray = [];
+for (let number of a) {
+    if (a.indexOf(number) !== a.lastIndexOf(number))
+        multiplesArray.push(number);
+}
+if (multiplesArraySorted = multiplesArray.sort(function (a, b) { return a - b }))
+alert(" You HAVE entered multiple elements with the same value");
+else {
+    alert("You HAVE NOT entered multiple values with the same value!")
+}
+
