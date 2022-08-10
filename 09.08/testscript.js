bc94d48e6821d3198d28a3abfa7c7a55eeac29b0
Your Name <you@example.com>
2022-08-10 17:52:59 +0300

    final commit

--- 09.08/testscript.js
+++ 09.08/testscript.js
@@ -1,31 +1,31 @@
 function add() {
     const Number1 = document.getElementById("Number1").value;
     const Number2 = document.getElementById("Number2").value;
-    const calculation = document.getElementById("calculation").value;
+    const calculation = document.getElementById("calculation");
     calculation.innerText = Number(Number1) + Number(Number2);
-    alert("The result is" + calculation);
+    alert("The result is " + calculation.innerText);
 }
 
 function subtract () {
     const Number1 = document.getElementById("Number1").value;
     const Number2 = document.getElementById("Number2").value;
-    const calculation = document.getElementById("calculation").value;
-    calculation.innerText = Number(Number1) + Number(Number2);
-    alert("The result is" + calculation)
+    const calculation = document.getElementById("calculation");
+    calculation.innerText = Number(Number1) - Number(Number2);
+    alert("The result is " + calculation.innerText)
 }
 
 function multiply () {
     const Number1 = document.getElementById("Number1").value;
     const Number2 = document.getElementById("Number2").value;
-    const calculation = document.getElementById("calculation").value;
-    calculation.innerText = Number(Number1) + Number(Number2);
-    alert("The result is" + calculation)
+    const calculation = document.getElementById("calculation");
+    calculation.innerText = Number(Number1) * Number(Number2);
+    alert("The result is " + calculation.innerText)
 }
 
 function divide () {
     const Number1 = document.getElementById("Number1").value;
     const Number2 = document.getElementById("Number2").value;
-    const calculation = document.getElementById("calculation").value;
-    calculation.innerText = Number(Number1) + Number(Number2);
-    alert("The result is" + calculation)
+    const calculation = document.getElementById("calculation");
+    calculation.innerText = Number(Number1) / Number(Number2);
+    alert("The result is " + calculation.innerText)
 }
