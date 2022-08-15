a7cfd71d9eb31f78ce9cae7c159589773d9d7008
Your Name <you@example.com>
2022-08-15 20:52:05 +0300

    3rd commit

--- 11.08/Test/testscript.js
+++ 11.08/Test/testscript.js
@@ -1,47 +1,18 @@
-let UnitedKingdom = "England"; "Wales", "Scotland"; "Ireland";
-let BalticStates = "Latvia"; "Lithuania";"Estonia";
-let Jutland = "Finnland"; "Germany";
-let Scandinavia = "Sweeden"; "Norway"; "Finnland";
-
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+}
 
-  function filterCountriesByRegion(region) {
+function filterCountriesByRegion(region) {
     let countries = document.getElementById("dataToFilter");
 
     for (let country of countries.children) {
-	let countryRegion = country.children[1].innerText;
+        let countryRegion = country.children[2].innerText;
 
-	if (countryRegion == region) {
-	    country.style.display = "";
-	}
-	else {
-	    country.style.display = "none";
-	}
+        if (countryRegion == region || region == "All") {
+            country.style.display = "";
+        }
+        else {
+            country.style.display = "none";
+        }
     }
 }
-
-
-
-    /* function filterCountriesByRegion(region) {
-        let countries = document.getElementById("dataToFilter");
-    
-        for (let country of countries.children) {
-        let countryRegion = country.children[1].innerText;
-            
-        if (countryRegion == "UnitedKingdom") {
-            country.style.display = "England", "Ireland", "Wales", "Scotland";
-        }    
-        else if (countryRegion == BalticStates) {
-            country.style.display = "Latvia", "Lithuania", "Estonia";
-        }
-        else if (countryRegion == Jutland) {
-            country.style.display = "Finnland", "Germany";
-        }   
-        else if (countryRegion == Scandinavia) {
-            country.style.display = "Finnland", "Norway", "Sweeden";
-        }
-      }
-     }*/
-    
