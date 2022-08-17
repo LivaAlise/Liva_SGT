05b11727045968109f611f4f1804904e436734dd
Your Name <you@example.com>
2022-08-17 10:35:13 +0300

    2nd commit

---
+++ 16.08/script.js
@@ -0,0 +1,8 @@
+const previousOperand= document.querySelector("[previous-operand]")
+const dataCurrentOperand= document.querySelector("[current-operand]")
+const numberButtons = document.querySelectorAll("[number]")
+const operationButtons = document.querySelectorAll("[operation]")
+const acbutton = document.querySelector ("[all-clear]")
+const deleteButton = document.querySelector("[delete]")
+const equalsButton = document.querySelector("[equals]")
+
