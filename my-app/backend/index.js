2d50ea0e913badc5cf54c37431f53dfb4ff6198d
Your Name <you@example.com>
2022-08-28 20:57:40 +0300

    3rd commit

--- my-app/backend/index.js
+++ my-app/backend/index.js
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "js_08_01"
+    database: "javascript_august"
 })
 
 connection.connect((error) => {
@@ -38,18 +38,39 @@ app.post("/add-customer", (req, res) => {
         firstname: req.body.name,
         lastname: req.body.lastname,
         phone: req.body.phone,
-        email: req.body.email
+        email: req.body.email,
+        vip: req.body.vip == "Yes" ? 1 : 0
     };
 
-    // fs.readFile('AllCustomer.json', "utf8", (err, data) => {
-    //     const customer = JSON.parse(data);
-    //     customer.customer.push(newCustomer);
-    //     fs.writeFile('AllCustomer.json', JSON.stringify(customer, null, 3), function(err) {
-    //         res.send("Customer is added");
-    //     });
-    // })
-})
+    const sqlQuery = "INSERT INTO customer (firstname,lastname,email,phone,vip) VALUES (?,?,?,?,?)";
+    connection.query(sqlQuery, [newCustomer.firstname, newCustomer.lastname,
+        newCustomer.email, newCustomer.phone, newCustomer.vip
+    ], (error, results) => {
+        if (error)
+            throw error;
+        res.send(JSON.stringify({
+            "status": 200,
+            "error": null,
+            "response": "Customer ID : " + results.insertId + " created!"
+        }))
+    })})
+ 
+app.post ("/delete-customer", (req, res) => {
+    const sqlQuery = "DELETE FROM customer WHERE id=(?)";
+    connection.query(sqlQuery, [req.body.delete], (error, results) => {
+        if (error)
+            throw error;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": { affected: results.affectedRows } 
+}))})})
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 })
+
+    // fs.readFile('AllCustomers.json', "utf8", (err, data) => {
+    //     const customers = JSON.parse(data);
+    //     customers.customers.push(newCustomer);
+    //     fs.writeFile('AllCustomers.json', JSON.stringify(customers, null, 3), function(err) {
+    //         res.send("Customer is added");
+    //     });
+    // })
