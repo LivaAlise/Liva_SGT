2587bca8f51622e5ce740c20320a75cb2e36ffd6
Your Name <you@example.com>
2022-08-29 12:09:10 +0300

    last commit

--- my-app/frontend/script.js
+++ my-app/frontend/script.js
@@ -1,33 +1,33 @@
-$($.getJSON("http://localhost:5000/get-allCustomer",
-    function(data) {
+$ (
+  $.getJSON ('http://localhost:5000/get-allCustomer', function (data) {
+    const customer = data.response.customer;
+    const customerTabElement = $ ('#customerTab');
+    for (let i = 0; i < customer.length; i++) {
+      const customer = customer[i];
 
-        const customer = data.response.customer;
-        const customerTabElement = $("#customerTab");
-        for (let i = 0; i < customer.length; i++) {
-            const customer = customer[i];
+      const row = document.createElement ('tr');
 
-            const row = document.createElement("tr");
+      const firstName = document.createElement ('td');
+      firstName.innerHTML = customer.firstname;
+      row.append (firstName);
 
-            const firstName = document.createElement("td");
-            firstName.innerHTML = customer.firstname;
-            row.append(firstName);
+      const lastName = document.createElement ('td');
+      lastName.innerHTML = customer.lastname;
+      row.append (lastName);
 
-            const lastName = document.createElement("td");
-            lastName.innerHTML = customer.lastname;
-            row.append(lastName);
+      const email = document.createElement ('td');
+      email.innerHTML = customer.email;
+      row.append (email);
 
-            const email = document.createElement("td");
-            email.innerHTML = customer.email;
-            row.append(email);
+      const phone = document.createElement ('td');
+      phone.innerHTML = customer.phone;
+      row.append (phone);
 
-            const phone = document.createElement("td");
-            phone.innerHTML = customer.phone;
-            row.append(phone);
+      const vip = document.createElement ('td');
+      vip.innerHTML = Number (customer.vip) === 1 ? 'Yes' : 'No';
+      row.append (vip);
 
-            const vip = document.createElement("td");
-            vip.innerHTML = Number(customer.vip) === 1 ? 'Yes' : "No";
-            row.append(vip);
-
-            customerTabElement.append(row);
-        }
-    }))
+      customerTabElement.append (row);
+    }
+  })
+);
