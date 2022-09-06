c895f1aaa6599ae76eba25203ddf5a8c2772c418
Your Name <you@example.com>
2022-09-06 15:38:09 +0300

    1st commit

---
+++ reactApp/frontend/my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Customers from './Customers.js';
+function App() {
+  return (
+    <div className="App">
+      <Customers></Customers>
+    </div>
+  );
+}
+
+export default App;
